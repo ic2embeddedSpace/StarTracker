500777dfccdfc0f4821eebec603301e971235777
ic2 <cartersquared1@gmail.com>
2024-06-16 09:35:53 -0700

    Add image processing

--- MainPatternProject.py
+++ MainPatternProject.py
@@ -1,4 +1,4 @@
-#I2C
+#IC2
 # ------------------------------
 # ---*-------------*---------*--
 # ------\-------/---\-------/---
@@ -11,12 +11,25 @@
 #############################################################################################
 #Program main for Processing images of stars/constellations and producing a black and white #
 #print out of symbols for light spectra present within photo                                #
-#Author: I2C                                                                                #
+#Author: IC2                                                                                #
 #File Initiation Date: 6/15/2024                                                            #
 #Last Commit: 6/16/2024                                                                     #
 #############################################################################################
 
 #pseudocode update
 #Read in file
+#how to read in file?
 #evaluate light value in section
 #determine which symbol shall be created for that section
+
+#############################################################################################
+#StarTrackerMain() - Function that serves as main project call function. Computer based only#
+#at current moment                                                                          #
+#Author: IC2                                                                                #
+#############################################################################################
+
+def StarTrackerMain():
+    print("Ad Astra per Aspera")
+    return
+
+StarTrackerMain()
