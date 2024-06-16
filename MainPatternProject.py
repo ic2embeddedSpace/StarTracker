8c64666bcafee8d8f0c3921002b89f8f50f08a42
ic2 <cartersquared1@gmail.com>
2024-06-16 02:14:02 -0700

    Updated Format PseudoCode

--- MainPatternProject.py
+++ MainPatternProject.py
@@ -11,7 +11,9 @@
 #############################################################################################
 #Program main for Processing images of stars/constellations and producing a black and white #
 #print out of symbols for light spectra present within photo                                #
-#                                                                                           #
+#Author: I2C                                                                                #
+#File Initiation Date: 6/15/2024                                                            #
+#Last Commit: 6/16/2024                                                                     #
 #############################################################################################
 
 #pseudocode update
