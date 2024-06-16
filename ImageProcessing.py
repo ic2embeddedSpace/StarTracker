e1b57e2e355d4f5db1c4fac81836415e8a3aa1c5
ic2 <cartersquared1@gmail.com>
2024-06-16 16:27:45 -0700

    Framework outlined

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -19,6 +19,23 @@
 #Creation of ImageProcessingMain
 def ImageProcessingMain():
     print("Slice of Time, Tis only an image, Tis only the first of many")
+    getPhoto()
+    return
+
+#Bring photo into program
+def getPhoto():
+    print("Photo of stars")
+    return
+
+#Section photo out
+def processPhoto():
+    print("Section photo")
+    return
+
+#Print 2 dimensional coordinates, take light values from the photo and process/encode them
+#into pattern values
+def encodingPattern():
+    print("Purl, knit, yo, and ssk")
     return
 
 #Callout of ImageProcessingMain()
